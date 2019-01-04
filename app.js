3df05247b327062a0d8e90dfa8d0487f6c305bba
vanisher <vannisher@yahoo.com>
2019-01-04 15:09:51 +0800

    config info

--- app.js
+++ app.js
@@ -1,12 +1,32 @@
-const express = require('express')
+const express = require('express');
 
-const app = express()
+const path = require('path');
 
-const port = process.env.PORT || 8000;
+const crypto = require('crypto');
+
+const bodyparser = require('body-parser');
+
+const multer =  require('multer');
+
+const Grid = require('grids-stream');
+
+const GridFsStorage = require('multer-gridfs-storage');
+
+const methodOverride = require('method-override');
+
+
+const app = express();
+
+//Middleware
+app.use(bodyParser.json());
+app.use(methodOverride('_method'));
+app.set('view engine','ejs')
 
 app.get('/',(req,res) => {
    res.send('welcome ')
 })
+
+const port = process.env.PORT || 8000;
 
 app.listen(port,() => {
    console.log(`your server is runing at port ${port}`);
