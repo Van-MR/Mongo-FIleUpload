23031cbb53adda5764b7d6c2e4646f01bede5901
vanisher <vannisher@yahoo.com>
2019-01-04 15:50:11 +0800

    version 0.2

--- app.js
+++ app.js
@@ -1,91 +1,81 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const path = require('path');
-
 const crypto = require('crypto');
-
-const bodyparser = require('body-parser');
-
-const multer =  require('multer');
-
-const Grid = require('grids-stream');
-
+const mongoose = require('mongoose');
+const multer = require('multer');
 const GridFsStorage = require('multer-gridfs-storage');
-
+const Grid = require('gridfs-stream');
 const methodOverride = require('method-override');
-
 
 const app = express();
 
-//Middleware
+// Middleware
 app.use(bodyParser.json());
 app.use(methodOverride('_method'));
-app.set('view engine','ejs')
+app.set('view engine', 'ejs');
 
-//Mongo URI
-const mongURI = 'mongodb://127.0.0.1:27017/Mong'
+// Mongo URI
+const mongoURI = 'mongodb://127.0.0.1:27017/Mong';
 
-//create mongo conenction
-const cono = mongoose.createConnection(mongURI);
+// Create mongo connection
+const conn = mongoose.createConnection(mongoURI);
 
-//Init gfs
+// Init gfs
 let gfs;
 
-cono.once('open', () => {
-    //init stream
-    gfs = Grid(cono.db, mongoose.mongo);
-    gfs.collection('uploads')
-})
+conn.once('open', () => {
+  // Init stream
+  gfs = Grid(conn.db, mongoose.mongo);
+  gfs.collection('uploads');
+});
 
-//create storage engine
-
-const storage = new GridStorage({
-   url:mongURI,
-   file: (req,file) => {
-      return Promise((resolve,reject) => {
-          crypto.randomBytes(16,(err,buf) => {
-              if(err) {
-                return reject(err)
-              }
-
-              const filename = buf.toString('hex') + path.extname(file.originalname);
-              const fileInfo = {
-                filename: filename,
-                bucketName: 'uploads'
-              };
-              resolve(fileInfo);
-          });
+// Create storage engine
+const storage = new GridFsStorage({
+  url: mongoURI,
+  file: (req, file) => {
+    return new Promise((resolve, reject) => {
+      crypto.randomBytes(16, (err, buf) => {
+        if (err) {
+          return reject(err);
+        }
+        const filename = buf.toString('hex') + path.extname(file.originalname);
+        const fileInfo = {
+          filename: filename,
+          bucketName: 'uploads'
+        };
+        resolve(fileInfo);
       });
-   }
-})
+    });
+  }
+});
+const upload = multer({ storage });
 
-const upload = multer({ storage })
-
-app.get('/',(req,res) => {
+// @route GET /
+// @desc Loads form
+app.get('/', (req, res) => {
   gfs.files.find().toArray((err, files) => {
-   // Check if files
-   if (!files || files.length === 0) {
-     res.render('index', { files: false });
-   } else {
-     files.map(file => {
-       if (
-         file.contentType === 'image/jpeg' ||
-         file.contentType === 'image/png'
-       ) {
-         file.isImage = true;
-       } else {
-         file.isImage = false;
-       }
-     });
-     res.render('index', { files: files });
-   }
- });
-})
+    // Check if files
+    if (!files || files.length === 0) {
+      res.render('index', { files: false });
+    } else {
+      files.map(file => {
+        if (
+          file.contentType === 'image/jpeg' ||
+          file.contentType === 'image/png'
+        ) {
+          file.isImage = true;
+        } else {
+          file.isImage = false;
+        }
+      });
+      res.render('index', { files: files });
+    }
+  });
+});
 
-
-// @route GET /files
-// @desc Uploads file to DB
-
+// @route POST /upload
+// @desc  Uploads file to DB
 app.post('/upload', upload.single('file'), (req, res) => {
   // res.json({ file: req.file });
   res.redirect('/');
@@ -122,7 +112,6 @@ app.get('/files/:filename', (req, res) => {
   });
 });
 
-
 // @route GET /image/:filename
 // @desc Display Image
 app.get('/image/:filename', (req, res) => {
@@ -147,7 +136,6 @@ app.get('/image/:filename', (req, res) => {
   });
 });
 
-
 // @route DELETE /files/:id
 // @desc  Delete file
 app.delete('/files/:id', (req, res) => {
@@ -160,10 +148,6 @@ app.delete('/files/:id', (req, res) => {
   });
 });
 
+const port = 5000;
 
-
-const port = process.env.PORT || 8000;
-
-app.listen(port,() => {
-   console.log(`your server is runing at port ${port}`);
-})
+app.listen(port, () => console.log(`Server started on port ${port}`));
